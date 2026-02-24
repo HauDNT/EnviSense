bb35b74cfdcaeb4c674161e8d1a903819cd9a68e
HauDNT <tienhau.it@gmail.com>
2026-02-24 16:15:53 +0700

    Update: Edit README file

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
-#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <SPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define TFT_CS  5
+#define TFT_DC  2
+#define TFT_RST 4
+
+Adafruit_ST7735 tft_screen = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  tft_screen.initR(INITR_BLACKTAB);
+  tft_screen.setRotation(1);
+  tft_screen.fillScreen(ST7735_BLACK);
+
+  tft_screen.setCursor(20, 10);
+  tft_screen.setTextColor(ST7735_WHITE);
+  tft_screen.setTextSize(1);
+  tft_screen.print("Hello World 123!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  tft_screen.setCursor(15, 50);
+  tft_screen.setTextColor(ST7735_RED, ST7735_BLACK);
+  tft_screen.setTextSize(2);
+  tft_screen.print("INTERNET OF THINGS");
+  delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  tft_screen.setCursor(15, 50);
+  tft_screen.setTextColor(ST7735_BLUE, ST7735_BLACK);
+  tft_screen.setTextSize(2);
+  tft_screen.print("INTERNET OF THINGS");
+  delay(1000);
+
+  tft_screen.setCursor(15, 50);
+  tft_screen.setTextColor(ST7735_YELLOW, ST7735_BLACK);
+  tft_screen.setTextSize(2);
+  tft_screen.print("INTERNET OF THINGS");
+  delay(1000);
 }
